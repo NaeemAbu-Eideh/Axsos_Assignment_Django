138fe7e32d74bc24a6f5b98bd201f100a81a1008
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-10-06 18:35:34 +0300

    update assignment

--- Users_with_Templates/myapp/urls.py
+++ Users_with_Templates/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('add-user', views.add_user),
-    path('/error', views.error)
+    path('/error', views.error),
+    path('dell', views.dell),
 ]
--- Users_with_Templates/myapp/views.py
+++ Users_with_Templates/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import *
 def index(request):
+    if  'display' not in request.session:
+        request.session['display'] = "none"
     records = User.get_all_records()
     context = {
         "records": records
@@ -8,14 +10,22 @@ def index(request):
     return render(request, 'page.html', context)
 
 def add_user(request):
+    if(request.POST['fname'] == "" or request.POST['lname'] == "" or request.POST['email'] == "" or request.POST['age'] == ""):
+        request.session['display'] = "block"
+        return redirect("/")
     context = {
         'fname': request.POST['fname'],
         'lname': request.POST['lname'],
         'email': request.POST['email'],
-        'age': request.POST['age'],
+        'age': int(request.POST['age']),
     }
+    request.session['display'] = "none"
     User.create_record(context)
     return redirect("/")
 
 def error(request):
     return render(request, 'page.html')
+
+def dell(request):
+    request.session.flush()
+    return redirect('/')

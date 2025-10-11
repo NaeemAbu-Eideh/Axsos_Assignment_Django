2e7e050b89310e99b72aeda45885b6f3d5ee3efd
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-10-11 08:51:52 +0300

    add new assignment

--- Supoort_ORM2/sports_orm/leagues/models.py
+++ Supoort_ORM2/sports_orm/leagues/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q, Count
 class League(models.Model):
 	name = models.CharField(max_length=50)
 	sport = models.CharField(max_length=15)
@@ -25,9 +25,91 @@ def all_teams_in_atlantic_league():
 
 def players_in_boston_penguins():
     team = Team.objects.filter(team_name = "Penguins", location = "Boston")
-    if(len(team) == 0):
-        return []
-    players = team.all_players.all()
-    if(len(players) == 0):
-        return []
+    team_id = team.values_list('id', flat = True)
+    players = Player.objects.filter(curr_team__in = team_id)
+    return players
+
+def all_playesr_in_International_Collegiate_Baseball_Conference():
+    leagues = League.objects.filter(name = 'International Collegiate Baseball Conference')
+    leagues_id = leagues.values_list('id', flat = True)
+    teams = Team.objects.filter(league__in = leagues_id)
+    teams_id = teams.values_list('id', flat=True)
+    players = Player.objects.filter(curr_team__in = teams_id)
+    return players
+
+def get_players__in_American_Conference_of_Amature_Football():
+    leagues = League.objects.filter(name__contains = "American", sport = "Football")
+    leagues_id = leagues.values_list('id', flat = True)
+    teams = Team.objects.filter(league__in = leagues_id)
+    teams_id = teams.values_list('id', flat=True)
+    players = Player.objects.filter(curr_team__in = teams_id, last_name = 'Lopez')
+    return players
+
+def get_football_players():
+    leagues = League.objects.filter(sport = 'Football')
+    leagues_id = leagues.values_list('id', flat=True)
+    all_teams = Team.objects.filter(league__in= leagues_id)
+    all_teams_id = all_teams.values_list('id', flat=True)
+    all_players = Player.objects.filter(all_teams__in = all_teams_id)
+    return all_players
+
+
+def get_all_teams_contain_sophia_players():
+    players = Player.objects.filter(first_name = "Sophia")
+    players_id = players.values_list('id', flat=True)
+    teams = Team.objects.filter(curr_players__in = players_id)
+    return teams
+
+def get_all_leagues_contain_sophia_players():
+    players = Player.objects.filter(first_name = "Sophia")
+    players_id = players.values_list('id', flat=True)
+    teams = Team.objects.filter(curr_players__in = players_id)
+    teams_id = teams.values_list('id', flat=True)
+    leagues = League.objects.filter(teams__in = teams_id)
+    return leagues
+
+def get_flores_does_not_currently_play_in_washington_roughriders():
+    team = Team.objects.filter(location = 'Washington', team_name = 'Roughriders')
+    team_id = team.values_list('id', flat = True)
+    players = Player.objects.filter(last_name = 'Flores').exclude(curr_team__in = team_id)
+    return players
+
+def get_all_samuel_evans_teams():
+    player = Player.objects.filter(first_name = 'Samuel', last_name = 'Evans')
+    player_id = player.values_list('id', flat= True)
+    teams = Team.objects.filter(all_players__in = player_id)
+    return teams
+
+def get_Manitoba_Tiger_Cats_all_players():
+    team = Team.objects.filter(location = 'Manitoba', team_name = 'Tiger-Cats')
+    team_id = team.values_list('id', flat=True)
+    players = Player.objects.filter(all_teams__in = team_id)
+    return players
+
+def get_formerly_not_current_in_wichita_vikings():
+    team = Team.objects.filter(location = 'Wichita', team_name = 'Vikings')
+    team_id = team.values_list('id', flat = True)
+    players = Player.objects.filter(all_teams__in = team_id).exclude(curr_team__in = team_id)
+    return players
+
+def get_jacob_teams_without_current_team():
+    player = Player.objects.filter(first_name = 'Jacob', last_name = 'Gray')
+    player_id = player.values_list('id', flat=True)
+    teams = Team.objects.filter(all_players__in = player_id).exclude(curr_players__in = player_id)
+    return teams
+
+def players_ever_play_in_atlantic_federation_amature_besball_players():
+    league = League.objects.filter(name = 'Atlantic Federation Amateur Players', sport = 'Baseball')
+    league_id = league.values_list('id', flat = True)
+    teams = Team.objects.filter(league__in = league_id)
+    teams_id = teams.values_list('id', flat = True)
+    players = Player.objects.filter(first_name = 'Joshua', all_teams__in = teams_id)
+    return players
+
+def get_teams_had_12_or_more_players():
+    teams = Team.objects.annotate(number_of_players = Count('all_players', distinct=True)).filter(number_of_players__gte = 12)
+    return teams
+
+def get_count_of_teams_for_each_player():
+    players = Player.objects.annotate(num_teams = Count('all_teams', distinct=True)).order_by('num_teams')
     return players
--- Supoort_ORM2/sports_orm/leagues/views.py
+++ Supoort_ORM2/sports_orm/leagues/views.py
@@ -7,6 +7,19 @@ def index(request):
 	context = {
                 'atlantic_soccor_conference_teams': models.all_teams_in_atlantic_league(),
                 'players_in_boston_penguins': models.players_in_boston_penguins(),
+                'playesr_in_International_Collegiate_Baseball_Conference': models.all_playesr_in_International_Collegiate_Baseball_Conference(),
+                'players__in_American_Conference_of_Amature_Football': models.get_players__in_American_Conference_of_Amature_Football(),
+                'football_players': models.get_football_players(),
+                'teams_contain_sophia_players': models.get_all_teams_contain_sophia_players(),
+                'leagues_contain_sophia_players': models.get_all_leagues_contain_sophia_players(),
+                'flores_does_not_currently_play_in_washington_roughriders': models.get_flores_does_not_currently_play_in_washington_roughriders(),
+                'all_samuel_evans_teams': models.get_all_samuel_evans_teams(),
+                'Manitoba_Tiger_Cats_all_players': models.get_Manitoba_Tiger_Cats_all_players(),
+                'formerly_not_current_in_wichita_vikings': models.get_formerly_not_current_in_wichita_vikings(),
+                'jacob_teams_without_current_team': models.get_jacob_teams_without_current_team(),
+                'joshua_in_atlantic_federation_amature_besball_players': models.players_ever_play_in_atlantic_federation_amature_besball_players(),
+                'had_12_or_more_players': models.get_teams_had_12_or_more_players(),
+                'count_of_teams_for_each_player': models.get_count_of_teams_for_each_player()
 	}
 	return render(request, "leagues/index.html", context)
 
@@ -15,4 +28,4 @@ def make_data(request):
 	team_maker.gen_teams(50)
 	team_maker.gen_players(200)
 
-	return redirect("index") 
+	return redirect("index")
